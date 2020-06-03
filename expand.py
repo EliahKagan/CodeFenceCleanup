bb39050e1caec2f3a7257c062ae14a46ee6241b5
Eliah Kagan <degeneracypressure@gmail.com>
2020-06-03 14:39:45 -0400

    initial commit
    basic idea

---
+++ expand.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Expand (what's intended to be a) a T-SQL expression for searching."""
+
+
+def expand(text, pattern, replacement, min_count, max_count):
+    """Expands \n and a single replacement with repetitions."""
+    for count in range(min_count, max_count + 1):
+        print(text.replace(pattern, replacement * count)
+                  .replace(r'\n', "' + CHAR(10) + '"))
+
+
+if __name__ == '__main__':
+    expand(text=r"OR ph.Text LIKE '%[`~][`~][`~]{}[^a-z\n]%'",
+           pattern=r'{}',
+           replacement=r'[^\n]',
+           min_count=0,
+           max_count=5)  # 5 is not high enough. This is just for testing.
